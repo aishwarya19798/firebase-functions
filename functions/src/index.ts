1647e4b474a1e3bb1ef608f08cb3e841a83f13a7
Aishwarya Sharma <46526203+aishwarya19798@users.noreply.github.com>
2021-09-27 15:45:14 +0530

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -27,6 +27,7 @@ export const getCitiesWeather = functions.https.onRequest((req,res)=>{
     })
     .catch(e=>{
         console.log(e)
+        response.send('error',e)
     })
 })
 
@@ -41,14 +42,14 @@ export const updateDelhiWeather = functions.firestore.document('cities-weather/D
     return admin.messaging().sendToTopic('cities-weather/Delhi',payload);
 })
 
-// export const getDelhiWeather = functions.https.onRequest((request, response) => {
-//   admin.firestore().doc('cities-weather/Delhi').get()
-//   .then(snapshot=>{
-//     const data = snapshot.data()
-//     response.send(data)
-//   })
-//   .catch(e=> {
-//       console.log('error: ',e)
-//       response.send('error')
-//   })
-// });
+export const getDelhiWeather = functions.https.onRequest((request, response) => {
+  admin.firestore().doc('cities-weather/Delhi').get()
+  .then(snapshot=>{
+    const data = snapshot.data()
+    response.send(data)
+  })
+  .catch(e=> {
+      console.log('error: ',e)
+      response.send('error',e)
+  })
+});
